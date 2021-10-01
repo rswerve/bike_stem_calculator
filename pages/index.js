6c63e9ef2eeb1ccd59ddb5dcc93a2d46af0eb65b
Mazdak Atighi <maz@truss.works>
2021-09-30 21:52:36 -0500

    it moves

--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,50 @@
+import { useReducer } from "react";
+import { Slider } from "@mui/material";
 import styles from "../styles/Home.module.css";
 
+const initialState = {
+    stemXOrigin: 0,
+    stemYOrigin: 150,
+    stemLength: 50,
+    spacerHeight: 50,
+};
+
+const reducer = (state, action) => {
+    console.log("state: ", state);
+    console.log("action: ", action);
+    switch (action.slider) {
+        case "stem":
+            return { ...state, ...action, stemLength: action.value };
+    }
+};
+
 export default function Home() {
+    const [state, setState] = useReducer(reducer, initialState);
     return (
         <div className={styles.container}>
+            <Slider
+                name="stem"
+                defaultValue={state.stemLength}
+                value={state.stemLength}
+                aria-label="Default"
+                valueLabelDisplay="auto"
+                onChange={(event, value) =>
+                    // console.log(event, value) ||
+                    setState({ slider: event.target.name, value: value })
+                }
+            />
             <svg
                 width="200"
                 height="250"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
             >
-                <rect
-                    x="10"
-                    y="10"
-                    width="30"
-                    height="30"
-                    stroke="black"
-                    fill="transparent"
-                    strokeWidth="5"
-                />
-                <rect
-                    x="60"
-                    y="10"
-                    rx="10"
-                    ry="10"
-                    width="30"
-                    height="30"
-                    stroke="black"
-                    fill="transparent"
-                    strokeWidth="5"
-                />
-
-                <circle
-                    cx="25"
-                    cy="75"
-                    r="20"
-                    stroke="red"
-                    fill="transparent"
-                    strokeWidth="5"
-                />
-                <ellipse
-                    cx="75"
-                    cy="75"
-                    rx="20"
-                    ry="5"
-                    stroke="red"
-                    fill="transparent"
-                    strokeWidth="5"
-                />
-
                 <line
-                    x1="10"
-                    x2="50"
-                    y1="110"
-                    y2="150"
+                    x1={state.stemXOrigin}
+                    y1={state.stemYOrigin}
+                    x2="200"
+                    y2={state.stemYOrigin + state.stemLength}
                     stroke="orange"
-                    strokeWidth="5"
-                />
-                <polyline
-                    points="60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145"
-                    stroke="orange"
-                    fill="transparent"
-                    strokeWidth="5"
-                />
-
-                <polygon
-                    points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
-                    stroke="green"
-                    fill="transparent"
-                    strokeWidth="5"
-                />
-
-                <path
-                    d="M20,230 Q40,205 50,230 T90,230"
-                    fill="none"
-                    stroke="blue"
                     strokeWidth="5"
                 />
             </svg>
