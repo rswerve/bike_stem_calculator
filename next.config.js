9e28cb86d0a8c459ed9fe3a1333ee992efcdead5
Mazdak Atighi <maz@truss.works>
2021-10-12 21:05:35 -0500

    more tweaks to next config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     webpack5: true,
     webpack: (config) => {
-        config.resolve.fallback = { fs: false };
+        config.resolve.fallback = { fs: false, path: false };
 
         return config;
     },
