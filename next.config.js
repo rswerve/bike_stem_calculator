79ea68d9a60c08d697ba8d0d7008fbc6b83669b3
Mazdak Atighi <maz@truss.works>
2021-10-12 21:03:28 -0500

    update next config to fix build error

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ module.exports = {
         locales: ["en"],
         defaultLocale: "en",
     },
+    webpack5: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+
+        return config;
+    },
 };
